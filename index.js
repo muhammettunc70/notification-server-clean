a99d5709b7826b3ff49590a70922614b12074879
Muhammet TUNÇ <muhammettunc@192.168.1.107>
2025-04-04 23:22:03 +0300

    firebaseKey artık env üzerinden okunuyor

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 app.use(express.json());
 
-const serviceAccount = require("./firebaseKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
